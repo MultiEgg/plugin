b1d3e6464dbe46423abc00da2a4a55e6f46b2d70
ExerpimentalX <chrlew082@gmail.com>
2023-03-12 18:57:08 -0600

    Update MultiEggController.php

--- app/Http/Controllers/Admin/MultiEggController.php
+++ app/Http/Controllers/Admin/MultiEggController.php
@@ -32,30 +32,47 @@ class MultiEggController extends Controller
         $key = DB::select("select * from `multiegg` where `id`='1'");
         return $this->view->make('admin.multiegg.index',[
                 'version'=>$this->version,
-                'key'=>$key
+                'key'=>$key,
+                'is_valid'=>$this->checkKeyValid()
         ]);
     }
 
     public function updateKeys(MultiEgg $multiegg, Request $request) {
         MultiEgg::where('id',1)->update(['license'=>$request->key]);
         if(Cache::has('multiegg_license_data')){ Cache::forget('multiegg_license_data'); }
+        if(Cache::has('multiegg_license_brand')){ Cache::forget('multiegg_license_brand'); }
+        if(Cache::has('multiegg_license_perm')){ Cache::forget('multiegg_license_perm'); }
+        if(Cache::has('multiegg_license_toggle')){ Cache::forget('multiegg_license_toggle'); }
         $this->cacheLicenseDetails();
         return redirect()->route('admin.multiegg.index');
     }
 
     public function checkKeyValid() {
-        if(!Cache::has('multiegg_license_data')) { $this->cacheLicenseDetails; }
-        $data = $this->cacheLicenseDetails();
+        if(!Cache::has('multiegg_license_data_key')) { $this->cacheLicenseDetails(); }
+        $data = Cache::get('multiegg_license_data_key');
+        if($data->error == (!isset($data->error) || null || true)) { return "error"; }
+        if($data->data->suspended == (!isset($data->data->suspended) || null || "1" || 1)) { return "suspended"; }
+        return "1";
     }
 
     public function cacheLicenseDetails() {
         if(!Cache::has('multiegg_license_data')) {
             $key = DB::select("select * from `multiegg` where `id`='1'");
-            $build_url = "https://api.dev.multiegg.xyz/api/key/{$key[0]->license}/info/key";
-            $res = Http::timeout(30)->get($build_url)->object();
-            Cache::put('multiegg_license_data', $res, now()->addMinutes(60));
+            $key_info = "https://api.dev.multiegg.xyz/api/key/{$key[0]->license}/info/key";
+            $brand_info = "https://api.dev.multiegg.xyz/api/key/{$key[0]->license}/info/branding";
+            $perm_info = "https://api.dev.multiegg.xyz/api/key/{$key[0]->license}/info/perms";
+            $toggle_info = "https://api.dev.multiegg.xyz/api/key/{$key[0]->license}/info/gtoggle";
+
+            $key_res = Http::timeout(30)->get($key_info)->object();
+            $brand_res = Http::timeout(30)->get($brand_info)->object();
+            $perm_res = Http::timeout(30)->get($perm_info)->object();
+            $toggle_res = Http::timeout(30)->get($toggle_info)->object();
+
+            Cache::put('multiegg_license_data_key', $key_res, now()->addMinutes(60));
+            Cache::put('multiegg_license_data_brand', $brand_res, now()->addMinutes(60));
+            Cache::put('multiegg_license_data_perm', $perm_res, now()->addMinutes(60));
+            Cache::put('multiegg_license_data_toggle', $toggle_res, now()->addMinutes(60));
         }
-        return Cache::get('multiegg_license_data');
     }
 
 }
